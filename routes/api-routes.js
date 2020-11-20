0b1ab6802310873fa741e936e00c4c5f52943213
Muhammed Ekinci <siraceddinekinci@gmail.com>
2020-11-19 20:01:51 -0500

    final commit before deploy

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,25 +3,25 @@ const db = require("../models");
 module.exports = function(app) {
     //display last workout on home page
     app.get("/api/workouts", function(req, res) {
-        db.Workout.find({}).then(function(dbWorkouts) {
+        db.Workout.find().then(function(dbWorkouts) {
             res.json(dbWorkouts);
         });
     });
     //add a new workout exercise
-    app.post("/api/workouts", function({ body }, res) {
-        db.Workout.create(body).then(function(dbWorkouts) {
+    app.post("/api/workouts", function(req, res) {
+        db.Workout.create({}).then(function(dbWorkouts) {
             res.json(dbWorkouts);
         });
     });
     //update of workout exercise
     app.put("/api/workouts/:id", function(req, res) {
-        db.Workout.findOneAndUpdate({_id:req.params.id}, {$set:  {exercises: req.body}}, { new: true } ).then(function(dbWorkouts) {
+        db.Workout.findByIdAndUpdate(req.params.id, {$push:  {exercises: req.body}}, { new: true } ).then(function(dbWorkouts) {
             res.json(dbWorkouts);
         });
     });
     //display stats graph of all performed wourkout exercises
     app.get("/api/workouts/range", function(req, res) {
-        db.Workout.find({}).populate("workouts").then(function(dbWorkouts) {
+        db.Workout.find({}).limit(7).then(function(dbWorkouts) {
             res.json(dbWorkouts);
         });
     });
